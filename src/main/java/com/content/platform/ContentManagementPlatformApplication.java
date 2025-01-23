c7627adb0acd47ac18a936e9d68cf47ffca2c973
abhi07799 <kumawatabhi7799@gmail.com>
2025-01-23 20:03:15 +0530

    Initial Commit

---
+++ src/main/java/com/content/platform/ContentManagementPlatformApplication.java
@@ -0,0 +1,15 @@
+package com.content.platform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ContentManagementPlatformApplication
+{
+
+    public static void main(String[] args)
+    {
+        SpringApplication.run(ContentManagementPlatformApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/content/platform/ContentManagementPlatformApplicationTests.java
@@ -0,0 +1,15 @@
+package com.content.platform;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ContentManagementPlatformApplicationTests
+{
+
+    @Test
+    void contextLoads()
+    {
+    }
+
+}
